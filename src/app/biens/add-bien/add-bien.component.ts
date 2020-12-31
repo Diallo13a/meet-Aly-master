ad352df29847eec95aab4ecf730abaa995004ce9
Oumar Diallo <oumar94diallo@gmail.com>
2020-12-31 15:33:47 +0100

    "31-dec-2020"

--- src/app/biens/add-bien/add-bien.component.ts
+++ src/app/biens/add-bien/add-bien.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {FormArray, FormControl, FormGroup, Validators} from '@angular/forms';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-add-bien',
@@ -9,6 +10,8 @@ import {FormArray, FormControl, FormGroup, Validators} from '@angular/forms';
 export class AddBienComponent implements OnInit {
   bienForm: FormGroup;
   options = ['Location', 'Vente'];
+  // the forbidden titles
+  forbidenTitles: string[] = ['Hamo', 'Village', 'Ville'];
   constructor() { }
 
   ngOnInit(): void {
@@ -18,13 +21,17 @@ export class AddBienComponent implements OnInit {
         null,
         [Validators.required,
           Validators.minLength(5),
-        Validators.maxLength(50)]),
+        Validators.maxLength(50),
+        ],
+        this.forbiddenValidator.bind(this)
+      ),
       price: new FormControl(
         null,
         [Validators.required,
-          Validators.min(100)
+          Validators.min(100),
+          this.lastPriceValidator
         ]),
-      description: new FormControl(null,null,),
+      description: new FormControl(null, null),
       categorie: new FormControl(null), // select
       image: new FormControl(null),
       sale: new FormControl('Location'), // radio
@@ -33,6 +40,28 @@ export class AddBienComponent implements OnInit {
     this.bienForm.patchValue({
       description: 'J\'ai terminé les formulaires',
     });
+  }
+  // Async Validator function
+  lastPriceValidator(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (+control.value > 1000000) {
+          resolve({lastPrice: true});
+          return {lastPrice: true};
+        }else{
+          resolve (null);
+        }
+        return null;
+      }, 2000);
+    });
+    return promise;
+  }
+  // Validator function
+  forbiddenValidator(ctrl: FormControl): {[key: string]: boolean}{
+    if (this.forbidenTitles.indexOf(ctrl.value) !== -1){
+      return {titleForbidden: true};
+    }
+    return null;
   }
   getimagesSecControl(): any{
     return (<FormArray> this.bienForm.get('imagesSec')).controls;
